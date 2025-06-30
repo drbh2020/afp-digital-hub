2d00f5f9ba374a0b34468cb4ffb583c25e4c14f0
Diego Raul Berastain Hermoza <berastaindiego@gmail.com>
2025-06-30 05:21:33 -0500

    chore: added babel plugin config for disabling explicit import of React module

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      "babel-preset-gatsby",
+      {
+        "targets": {
+          "browsers": [">0.25%", "not dead"]
+        }
+      }
+    ]
+  ],
+  plugins: [
+    [
+      "@babel/plugin-transform-react-jsx",
+      {
+        "runtime": "automatic"
+      }
+    ]
+  ]
+};
