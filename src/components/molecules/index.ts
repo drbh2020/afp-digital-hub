91307b765e2f7e51744f0123b21aba2e7cd0bf2d
Diego Raul Berastain Hermoza <berastaindiego@gmail.com>
2025-06-30 05:25:12 -0500

    chore: added molecules barrel file

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,6 @@
+// Molecular components - Combinations of atoms
+export { FormField } from './FormField/FormField';
+export type { FormFieldProps } from './FormField/FormField';
+
+export { Card } from './Card/Card';
+export type { CardProps } from './Card/Card';
