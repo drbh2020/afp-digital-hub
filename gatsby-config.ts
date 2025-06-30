8d892a99a49dc0ad47d27dd6bbce0a8bd2269de2
Diego Raul Berastain Hermoza <berastaindiego@gmail.com>
2025-06-30 05:22:26 -0500

    feat: added and configured favicons in gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -43,17 +43,91 @@ const config: GatsbyConfig = {
         background_color: `#ff4f00`,
         theme_color: `#ff4f00`, // https://css-tricks.com/meta-theme-color-and-trickery/
         display: `minimal-ui`,
-        icon: `static/images/icons/favicon-512x512.png`, // This path is relative to the root of the site.
+        icon: `static/images/favicon/favicon-96x96.png`, // This path is relative to the root of the site.
         icons: [
           {
-            src: "static/images/icons/favicon-192x192.png",
+            src: "static/images/favicon/android-icon-36x36.png",
+            sizes: "36x36",
+            type: "image/png"
+          },
+          {
+            src: "static/images/favicon/android-icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "static/images/favicon/android-icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "static/images/favicon/android-icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "static/images/favicon/android-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "static/images/favicon/android-icon-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "static/images/icons/favicon-512x512.png", 
-            sizes: "512x512",
-            type: "image/png"
+            src: "static/images/favicon/apple-icon-57x57.png",
+            sizes: "57x57",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-114x114.png",
+            sizes: "114x114",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "static/images/favicon/apple-icon-180x180.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any maskable"
           }
         ]
       },
