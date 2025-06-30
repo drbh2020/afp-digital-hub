38b2866f5b28f5fc64c1308f0ca6c3021b4bbc88
Diego Raul Berastain Hermoza <berastaindiego@gmail.com>
2025-06-30 05:26:04 -0500

    chore: added organisms barrel file

---
+++ src/components/organisms/index.ts
@@ -0,0 +1,9 @@
+// Organisms exports
+export { Header } from './Header/Header';
+export type { HeaderProps } from './Header/Header';
+
+export { Footer } from './Footer/Footer';
+export type { FooterProps } from './Footer/Footer';
+
+export { CalculatorForm } from './CalculatorForm/CalculatorForm';
+export { CalculatorErrorBoundary } from './CalculatorForm/CalculatorFormErrorBoundary';
