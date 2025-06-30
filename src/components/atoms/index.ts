2fea1c2dffc900d26c9a8a5604a4879d3e862377
Diego Raul Berastain Hermoza <berastaindiego@gmail.com>
2025-06-30 05:24:27 -0500

    chore: added atoms barrel file

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,9 @@
+// Atomic components - Basic building blocks
+export { Button } from './Button/Button';
+export type { ButtonProps } from './Button/Button';
+
+export { Input } from './Input/Input';
+export type { InputProps } from './Input/Input';
+
+export { Text } from './Text/Text';
+export type { TextProps } from './Text/Text';
