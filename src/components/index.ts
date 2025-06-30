48768b7913b5d13d2f79fa6ff5d38be576fc9880
Diego Raul Berastain Hermoza <berastaindiego@gmail.com>
2025-06-30 05:28:40 -0500

    chore: added atomic design barrel file

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+// Atomic Design System Components
+
+// Atoms
+export * from './atoms';
+
+// Molecules
+export * from './molecules';
+
+// Organisms
+export * from './organisms';
+
+// Templates
+export * from './templates';
